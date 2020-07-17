d74f9925c1da0238003b1a104e2d42fc9a325e11
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-17 11:00:15 -0700

    test adding quantity property to pastry class and pass

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,14 +9,14 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastryClass_Pastry()
     {
-      Pastry pastryOrder = new Pastry();
+      Pastry pastryOrder = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
     }
     [TestMethod]
     public void GetPastryQuantity_ReturnsPastryQuantity_Int()
     {
       int pastryQuantity = 1;
-      Pastry pastryOrder = new Pastry();
+      Pastry pastryOrder = new Pastry(1);
       int result = pastryOrder.PastryQuantity;
       Assert.AreEqual(pastryQuantity, result);
     }
