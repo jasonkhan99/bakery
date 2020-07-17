9bab1c5243929ed3a4149a10b4770c7c56f9651b
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-17 14:06:49 -0700

    test subtract bread discount and fail

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,22 +9,28 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBreadClass_Bread()
     {
-      Bread breadOrder = new Bread(1);
+      Bread breadOrder = new Bread(3);
       Assert.AreEqual(typeof(Bread), breadOrder.GetType());
     }
     [TestMethod]
     public void GetBreadQuantity_ReturnsBreadQuantity_Int()
     {
-      int breadQuantity = 1;
-      Bread breadOrder = new Bread(1);
+      int breadQuantity = 3;
+      Bread breadOrder = new Bread(3);
       int result = breadOrder.BreadQuantity;
       Assert.AreEqual(breadQuantity, result);
     }
     [TestMethod]
     public void AddBreadCost_ReturnBreadCost_Int()
     {
-      Bread breadOrder = new Bread(1);
-      Assert.AreEqual(5, breadOrder.AddBreadCost());
+      Bread breadOrder = new Bread(3);
+      Assert.AreEqual(15, breadOrder.AddBreadCost());
+    }
+    [TestMethod]
+    public void SubtractBreadDiscount_ReturnBreadDiscount_Int()
+    {
+      Bread breadOrder = new Bread(3);
+      Assert.AreEqual(5, breadOrder.SubtractBreadDiscount(15));
     }
   }
 }
--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,22 +9,22 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastryClass_Pastry()
     {
-      Pastry pastryOrder = new Pastry(1);
+      Pastry pastryOrder = new Pastry(3);
       Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
     }
     [TestMethod]
     public void GetPastryQuantity_ReturnsPastryQuantity_Int()
     {
-      int pastryQuantity = 1;
-      Pastry pastryOrder = new Pastry(1);
+      int pastryQuantity = 3;
+      Pastry pastryOrder = new Pastry(3);
       int result = pastryOrder.PastryQuantity;
       Assert.AreEqual(pastryQuantity, result);
     }
     [TestMethod]
     public void AddPastryCost_ReturnPastryCost_Int()
     {
-      Pastry pastryOrder = new Pastry(1);
-      Assert.AreEqual(2, pastryOrder.AddPastryCost());
+      Pastry pastryOrder = new Pastry(3);
+      Assert.AreEqual(6, pastryOrder.AddPastryCost());
     }
   }
 }
